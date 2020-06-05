97ece15a208694f5335c7e7d1575effa036e3e0b
giapvanlinh <linhgv3785@gmail.com>
2020-06-05 17:34:09 +0700

    update rating

---
+++ frontend/index.php
@@ -0,0 +1,220 @@
+<?php 
+	include('./asset/re-header.php');
+ ?>
+		<div id="wrap-inner">
+			<div class="products">
+				<h3>Điện thoại</h3>
+				<div class="product-list row">
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/apple-iphone-11-64gb.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Apple iPhone 11 64GB</a></p>
+						<p class="price">14.700.000₫</p>	  
+						<div class="marsk">
+							<a href="./details.php">Xem chi tiết</a>
+						</div>                                    
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/apple-iphone-11-pro-max-64gb.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Apple Iphone 11 Pro Max 64GB</a></p>
+						<p class="price">25.790.000₫</p>	  
+						<div class="marsk">
+							<a href="./details.php">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/samsung-galaxy-a51-a515.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Samsung Galaxy A51 A515</a></p>
+						<p class="price">5.800.000₫</p>	  
+						<div class="marsk">
+							<a href="./details.php">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/dien-thoai-nokia-6300.jpg" class="img-thumbnail"></a>
+						<p><a href="#">Điện thoại Nokia 6300</a></p>
+						<p class="price">200.000₫</p>	  
+						<div class="marsk">
+							<a href="details.php">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/apple-iphone-11-128gb.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Apple iPhone 11 128GB</a></p>
+						<p class="price">17.699.000₫</p>	  
+						<div class="marsk">
+							<a href="details.php">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/samsung-galaxy-s7-edge.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Samsung Galaxy S7 Edge</a></p>
+						<p class="price">2.980.000₫</p>	  
+						<div class="marsk">
+							<a href="details.php">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/samsung-galaxy-a01.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Samsung Galaxy A01</a></p>
+						<p class="price">1.790.000₫</p>	  
+						<div class="marsk">
+							<a href="details.php">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/samsung-galaxy-a71-a715.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Samsung Galaxy A71 A715</a></p>.
+						<p class="price">7.750.000₫</p>	  
+						<div class="marsk">
+							<a href="details.php">Xem chi tiết</a>
+						</div>                      	                        
+					</div> 
+				</div>                	                	
+			</div>
+
+			<div class="products">
+				<h3>Lap Top</h3>
+				<div class="product-list row">
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-01.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Apple Macbook Air 2019 MVFK2/MVFJ2/MVFJ2</a></p>
+						<p class="price">28.048.980₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-02.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Laptop WeiPai Book a7 Z8350</a></p>
+						<p class="price">3.999.000₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-03.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Máy tính bảng SURFACE PRO 6 I5 256GB</a></p>
+						<p class="price">21.900.000₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-04.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Microsoft Surface Pro 6 128GB</a></p>
+						<p class="price">20.790.000₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-05.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">MacBook Air MQD32 13.3INCH</a></p>
+						<p class="price">15.250.000₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-06.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Laptop Dell Vostro 5581 70175952/70175950</a></p>
+						<p class="price">13.990.000₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-07.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Laptop Dell Vostro 3580 P75F010V80I</a></p>
+						<p class="price">13.179.000₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-08.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Laptop Asus A412FA-EK734T</a></p>
+						<p class="price">15.499.000₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                      
+					</div> 
+				</div>    
+			</div>
+
+			<div class="products">
+				<h3>Ti Vi</h3>
+				<div class="product-list row">
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-01.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Apple Macbook Air 2019 MVFK2/MVFJ2/MVFJ2</a></p>
+						<p class="price">28.048.980₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-02.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Laptop WeiPai Book a7 Z8350</a></p>
+						<p class="price">3.999.000₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-03.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Máy tính bảng SURFACE PRO 6 I5 256GB</a></p>
+						<p class="price">21.900.000₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-04.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Microsoft Surface Pro 6 128GB</a></p>
+						<p class="price">20.790.000₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-05.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">MacBook Air MQD32 13.3INCH</a></p>
+						<p class="price">15.250.000₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-06.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Laptop Dell Vostro 5581 70175952/70175950</a></p>
+						<p class="price">13.990.000₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-07.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Laptop Dell Vostro 3580 P75F010V80I</a></p>
+						<p class="price">13.179.000₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                        
+					</div>
+					<div class="product-item col-md-3 col-sm-6 col-xs-12">
+						<a href="#"><img src="img/home/index-laptop/laptop-08.jpeg" class="img-thumbnail"></a>
+						<p><a href="#">Laptop Asus A412FA-EK734T</a></p>
+						<p class="price">15.499.000₫</p>	  
+						<div class="marsk">
+							<a href="#">Xem chi tiết</a>
+						</div>                      	                      
+					</div> 
+				</div>    
+			</div>
+		</div>
+
+		
+
+<?php 
+	include('./asset/footer.php');
+ ?>
